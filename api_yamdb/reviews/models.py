f299509c00fe37faeb2b0c4274a54f96508b5cc9
Vlad Nikitin <nikitinv91@gmail.com>
2021-11-02 19:11:26 +0300

    models skeleton added

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,33 @@
+import uuid
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    # поле role сделвть read_only в сериализаторе
+    bio: str = models.TextField('About user', blank=True, null=True)
+    role: str = models.CharField(
+        'User role', choices=['admin', 'moderator', 'user'])
+    confirmation_code: str = models.CharField(
+        max_length=12, default=uuid.uuid4)
+
+
+class Review(models.Model):
+    pass
+
+
+class Title(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
+
+
+class Genre(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
